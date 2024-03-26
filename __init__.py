09a207103cb3f34b979393dcbfc90eb519a76e69
RAJ VERMA <rajvermasja18@gmail.com>
2024-03-26 12:34:21 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import spacy
+spacy.load('en_core_web_sm')
 spacy.load(r'c:\users\rajve\appdata\local\packages\pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0\localcache\local-packages\python312\site-packages\en_core_web_sm\en_core_web_sm-3.7.1')
 import nltk
 nltk.download("popular")
