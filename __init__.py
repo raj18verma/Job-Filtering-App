eaa43ad90db76ed050d577c919c328806ac9f6ca
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2024-03-26 11:05:38 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import spacy
 from spacy import displacy
-nlp=spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_sm')
 import nltk
 nltk.download("popular")
 nltk.download('stopwords')
